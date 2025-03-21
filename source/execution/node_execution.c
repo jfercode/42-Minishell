e7d3cbe3eef87b0cb48b37ede940b2ac3ec557de
penpalac <penelopepalacios@outlook.es>
2025-03-21 14:51:45 +0100

    2025-03-21 added pipe execution that actually works

--- source/execution/node_execution.c
+++ source/execution/node_execution.c
@@ -6,7 +6,7 @@
 /*   By: penpalac <penpalac@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 17:41:17 by jaferna2          #+#    #+#             */
-/*   Updated: 2025/03/21 14:49:14 by penpalac         ###   ########.fr       */
+/*   Updated: 2025/03/21 14:50:39 by penpalac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,72 +92,4 @@ void	execute_cmd_node(t_ast *node, int fd_in, int fd_out)
 	}
 	else
 		perror("fork");
-}
-
-/**
- *	type: PIPE
- *	token: |
- *	left: x
- *	right: y
- */
-
-void	forking(t_ast *node, int fdin, int fdout)
-{
-	pid_t	pid;
-
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("Error: fork failed");
-		exit(1);
-	}
-	if (pid == 0)
-	{
-		if (fdin != STDIN_FILENO)
-		{
-			dup2(fdin, STDIN_FILENO);
-			close(fdin);
-		}
-		if (fdout != STDOUT_FILENO)
-		{
-			dup2(fdout, STDOUT_FILENO);
-			close(fdout);
-		}
-		run_command(node);
-	}
-}
-
-void	execute_pipe_node(t_ast *node)
-{
-	// call pipe_function(x, y)
-	int		fd[2];
-	t_ast	*current;
-	int		prev_fd;
-
-	if (node->type != NODE_PIPE)
-		return ; 
-	prev_fd = STDIN_FILENO;
-	current = node;
-	while (current && current->type == NODE_PIPE)
-	{
-		if (pipe(fd) == -1)
-		{
-			perror("Error: pipe failed");
-			return ;
-		}
-		if (current->left && current->left->type == NODE_CMD)
-			forking(current->left, prev_fd, fd[1]);
-		else
-			execute_pipe_node(current->left);
-		close(fd[1]);
-		if (prev_fd != STDIN_FILENO)
-			close(prev_fd);
-		prev_fd = fd[0];
-		current = current->right;
-		if (current && current->type == NODE_CMD)
-			forking(current, prev_fd, STDOUT_FILENO);
-	}
-	if (prev_fd != STDIN_FILENO)
-		close(prev_fd);
-	wait(NULL);
 }
---
+++ source/execution/pipe_execution.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe_execution.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: penpalac <penpalac@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 14:46:22 by penpalac          #+#    #+#             */
+/*   Updated: 2025/03/21 14:50:26 by penpalac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+/**
+ *	type: PIPE
+ *	token: |
+ *	left: x
+ *	right: y
+ */
+
+void	forking(t_ast *node, int fdin, int fdout)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error: fork failed");
+		exit(1);
+	}
+	if (pid == 0)
+	{
+		if (fdin != STDIN_FILENO)
+		{
+			dup2(fdin, STDIN_FILENO);
+			close(fdin);
+		}
+		if (fdout != STDOUT_FILENO)
+		{
+			dup2(fdout, STDOUT_FILENO);
+			close(fdout);
+		}
+		run_command(node);
+	}
+}
+
+void	execute_pipeline(t_ast **cmds, int pipe_count, int *fd, int prev_fd)
+{
+	int	status;
+	int	i;
+
+	i = 0;
+	while (i < pipe_count)
+	{
+		if (pipe(fd) == -1)
+		{
+			perror("pipe");
+			exit(EXIT_FAILURE);
+		}
+		forking(cmds[i], prev_fd, fd[1]);
+		close(fd[1]);
+		if (prev_fd != STDIN_FILENO)
+			close(prev_fd);
+		prev_fd = fd[0];
+		i++;
+	}
+	forking(cmds[i], prev_fd, STDOUT_FILENO);
+	if (prev_fd != STDIN_FILENO)
+		close(prev_fd);
+	while (wait(&status) > 0);
+}
+
+t_ast	**order_cmds(t_ast *node, t_ast **cmds)
+{
+	t_ast	*temp;
+	t_ast	*current;
+	int		i;
+	int		j;
+
+	i = 0;
+	current = node;
+	while (current && current->type == NODE_PIPE)
+	{
+		cmds[i++] = current->right;
+		current = current->left;
+	}
+	cmds[i++] = current;
+	cmds[i] = NULL;
+	j = 0;
+	while (j < i / 2)
+	{
+		temp = cmds[j];
+		cmds[j] = cmds[i - 1 - j];
+		cmds[i - 1 - j] = temp;
+		j++;
+	}
+	return (cmds);
+}
+
+void	execute_pipe(t_ast *node)
+{
+	t_ast	**cmds;
+	t_ast	*current;
+	int		fd[2];
+	int		prev_fd;
+	int		pipe_count;
+
+	prev_fd = STDIN_FILENO;
+	current = node;
+	pipe_count = 0;
+	while (current && current->type == NODE_PIPE)
+	{
+		pipe_count++;
+		current = current->left;
+	}
+	cmds = malloc((pipe_count + 2) * sizeof(t_ast *));
+	if (!cmds)
+		return ;
+	cmds = order_cmds(node, cmds);
+	execute_pipeline(cmds, pipe_count, fd, prev_fd);
+	free(cmds);
+}
